2821f09a7a8e319e036330b9d35d2c17ac33d5bb
HassunMohsyn <hassun9992@gmail.com>
2025-05-02 22:54:24 +0300

    first push

---
+++ src/Components/CardContainer/CardContainer.js
@@ -0,0 +1,25 @@
+import { Col, Container, Row } from "react-bootstrap";
+import NewCard from "../Card/Card";
+// import NewCard from "../Card/Card";
+
+export default function CardContainer({listImg}) {
+    return(
+        <Container>
+            <Row>
+
+        {listImg.map((item,index) => {
+            return(
+                
+                <Col key={index}>
+                <NewCard img={item.img} title={item.title} text={item.text} price={item.price}/>
+                </Col>
+            )
+        })}
+        </Row>
+
+        </Container>
+)
+    
+        
+    
+}
---
+++ src/Components/cardImages/CardImages.js
@@ -0,0 +1,22 @@
+import { Card, Col, Container, Row } from "react-bootstrap"
+
+export default function CardImages ({listImg}) {
+    return(
+        <Container>
+            <Row>
+
+        {listImg.map((item,index) => {
+            return(
+                
+                <Col style={{marginTop:`${index %2 === 0 ? '50px' : '0'}`}} key={index}>
+                    <Card className="card" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={item} />
+                    </Card>
+                </Col>
+            )
+        })}
+        </Row>
+
+        </Container>
+    )
+}
